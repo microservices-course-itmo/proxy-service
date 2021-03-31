5c1436496e3a69e18d1971358eb1a558d061d518
Semyon Kulikov <senich.99@gmail.com>
2021-03-31 21:24:23 +0300

    Deleted useless properties, fixed init script

--- src/main/resources/db/migration/V1.0.0__init.sql
+++ src/main/resources/db/migration/V1.0.0__init.sql
@@ -1,13 +1,9 @@
-DROP TABLE IF EXISTS proxies CASCADE;
 
 CREATE TABLE proxies
 (
     id  bigint PRIMARY KEY,
     url TEXT NOT NULL UNIQUE
 );
-
-DROP TABLE IF EXISTS parser_proxies CASCADE;
-
 CREATE TABLE parser_proxies
 (
     id bigint PRIMARY KEY,

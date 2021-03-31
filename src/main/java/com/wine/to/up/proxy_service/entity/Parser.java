7516509507e466b027ef48f2b015490d50bb8fd1
bgubanov <b.gubanov@list.ru>
2021-03-31 22:37:18 +0300

    Fix enum

--- src/main/java/com/wine/to/up/proxy_service/entity/Parser.java
+++ src/main/java/com/wine/to/up/proxy_service/entity/Parser.java
@@ -5,24 +5,19 @@ package com.wine.to.up.proxy_service.entity;
  * @since : 30.03.2021, вт
  **/
 public enum Parser {
-    AM_PARSER_SERVICE("am-parser-service", "https://amwine.ru"),
-    PARSER_SERVICE("crossroad-parser-service", "http://perekrestok-store.ru/"),
-    LENTA_PARSER_SERVICE("lenta-parser-service", "https://lenta.com/"),
-    SIMPLE_PARSER_SERVICE("simple-parser-service", "https://simplewine.ru/"),
-    WINESTYLE_PARSER_SERVICE("winestyle-parser-service", "https://spb.winestyle.ru/"),
-    WINELAB_PARSER_SERVICE("winelab-parser-service", "https://www.winelab.ru/");
+    AM_PARSER_SERVICE("https://amwine.ru"),
+    PARSER_SERVICE("http://perekrestok-store.ru/"),
+    LENTA_PARSER_SERVICE("https://lenta.com/"),
+    SIMPLE_PARSER_SERVICE("https://simplewine.ru/"),
+    WINESTYLE_PARSER_SERVICE("https://spb.winestyle.ru/"),
+    WINELAB_PARSER_SERVICE("https://www.winelab.ru/");
 
-    private final String name;
     private final String path;
 
-    Parser(String name, String path) {
-        this.name = name;
+    Parser(String path) {
         this.path = path;
     }
 
-    public String getName() {
-        return name;
-    }
 
     public String getPath() {
         return path;
